1d0106577c997cf057104f1251458774f55d98fd
Tomas Lenc <tomas.lenc@gmail.com>
2022-11-04 17:22:05 +0100

    adding tests

--- tests/run_tests.m
+++ tests/run_tests.m
@@ -1,4 +1,3 @@
 addpath(genpath('../src'))
 
-run(test_acf); 
-run(test_fft_normalization); 
+runtests
--- tests/test_acf.m
+++ tests/test_acf.m
@@ -2,12 +2,14 @@ function tests = test_acf
     tests = functiontests(localfunctions);
 end
 
+
 function test_acf_multidim(test_case)
 
     fs = 100; 
-    x = randn(1, fs * 60); 
+    x = pinknoise(fs * 60)'; 
+    x = x - min(x); 
     x_multi_dim = []; 
-    x_multi_dim(:, 1, 1, 1, 1, :) = [x; x*2]; 
+    x_multi_dim(:, 1, 1, 1, 1, :) = [x; 4.4 + 2 * x]; 
     [acf, lags, ap, mX, freq, ap_par] = get_acf(x_multi_dim, fs, ...
                                                'rm_ap', true, ...
                                                'f0_to_ignore', 1/2.4 ...
@@ -37,4 +39,54 @@ function test_acf_multidim(test_case)
     verifyEqual(test_case, squeeze(mX(2,1,1,1,1,:)), mX_2'); 
 
 end
+
+
+function test_mX_multidim(test_case)
+
+    fs = 100; 
+    x = pinknoise(fs * 60)'; 
+    x = x - min(x); 
+    x_multi_dim = []; 
+    x_multi_dim(:, 1, 1, 1, 1, :) = [x; 4.4 + 2 * x]; 
+    [~, ~, ~, mX] = get_acf(x_multi_dim, fs, ...
+                                               'rm_ap', true, ...
+                                               'f0_to_ignore', 1/2.4, ...
+                                               'fit_knee', true); 
+                                           
+    [~, ~, ~, mX_1]  = get_acf(squeeze(x_multi_dim(1,1,1,1,1,:))', fs, ...
+                   'rm_ap', true, ...
+                   'f0_to_ignore', 1/2.4, ...
+                   'fit_knee', true ...
+                   ); 
+               
+    verifyEqual(test_case, squeeze(mX(1,1,1,1,1,:)), mX_1'); 
+end
+
+
+function test_mX_equal_knee_no_knee(test_case)
+
+    fs = 100; 
+    x = pinknoise(fs * 60)'; 
+    x = x - min(x); 
+    [~, ~, ~, mX_1]  = get_acf(x, fs, ...
+                               'rm_ap', true, ...
+                               'f0_to_ignore', 1/2.4, ...
+                               'fit_knee', true ...
+                               ); 
+    [~, ~, ~, mX_2]  = get_acf(x, fs, ...
+                               'rm_ap', true, ...
+                               'f0_to_ignore', 1/2.4, ...
+                               'fit_knee', false ...
+                               ); 
+    verifyEqual(test_case, mX_1, mX_2); 
+end
+
+
+
+
+
+
+
+
+
 
