d5c232c72a8dab05fa1c88814dca5dc47704e743
Tomas Lenc <tomas.lenc@gmail.com>
2023-07-17 22:39:24 +0200

    fix: squeeze multidim inputs before amri_sig_fractal() call

--- tests/test_acf.m
+++ tests/test_acf.m
@@ -71,6 +71,42 @@ function test_acf_multidim(test_case)
 end
 
 
+function test_acf_multidim_irasa(test_case)
+
+    fs = 100; 
+    x = pinknoise(fs * 60)'; 
+    x = x - min(x); 
+    x_multi_dim = []; 
+    x_multi_dim(:, 1, 1, 1, 1, :) = [x; 4.4 + 2 * x]; 
+    [acf, lags, ap, mX, freq, ap_par] = get_acf(x_multi_dim, fs, ...
+                                               'rm_ap', true, ...
+                                               'f0_to_ignore', 1/2.4 ,...
+                                               'ap_fit_method', 'irasa' ...
+                                               ); 
+    expected_size = size(x_multi_dim); 
+    expected_size(end) = length(lags); 
+    assert(all(size(acf) == expected_size))
+    verifyNotEqual(test_case, squeeze(mX(1,1,1,1,1,:)), squeeze(mX(2,1,1,1,1,:)))
+    verifyNotEqual(test_case, squeeze(acf(1,1,1,1,1,:)), squeeze(acf(2,1,1,1,1,:)))
+
+    [acf_1, ~, ap_1, mX_1, ~, ap_par] = get_acf(squeeze(x_multi_dim(1,1,1,1,1,:))', fs, ...
+                   'rm_ap', true, ...
+                   'f0_to_ignore', 1/2.4, ...
+                   'ap_fit_method', 'irasa' ...
+                   ); 
+    [acf_2, ~, ap_2, mX_2] = get_acf(squeeze(x_multi_dim(2,1,1,1,1,:))', fs, ...
+                   'rm_ap', true, ...
+                   'f0_to_ignore', 1/2.4, ...
+                   'ap_fit_method', 'irasa' ...
+                   ); 
+
+    verifyEqual(test_case, squeeze(acf(1,1,1,1,1,:)), acf_1'); 
+    verifyEqual(test_case, squeeze(acf(2,1,1,1,1,:)), acf_2'); 
+    
+    verifyEqual(test_case, squeeze(ap(1,1,1,1,1,:)), ap_1'); 
+    verifyEqual(test_case, squeeze(ap(2,1,1,1,1,:)), ap_2'); 
+    
+end
 
 
 function test_acf_f0_ignore(test_case)
